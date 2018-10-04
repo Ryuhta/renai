5cbb5f4f0a6205029160d4328c4c344af9378572
Ryuta <r.giftedagent@gmail.com>
2018-10-04 11:20:25 +0900

    renai
    renairenai

---
+++ Assets/game1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game1 : MonoBehaviour {
+	//文字を書く場所を指定
+	public Rect zahyou = new Rect(0,0,100,50);
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	//GUI更新イベントが有ると勝手に呼ばれる
+	void OnGUI()
+	{
+		GUI.Label(zahyou,"にゃーん");   //文字を書く
+	}
+}
